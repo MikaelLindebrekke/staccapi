646ce5d920603eac2fe038324204e48c7a2e36ba
Mikael Lindebrekke <lindebrekkem@gmail.com>
2021-10-31 21:30:36 +0100

    Added the big pep.csv file

--- database.js
+++ database.js
@@ -17,7 +17,7 @@ async function getPerson(name) {
 function readCSV() {
     return new Promise((resolve, reject) => {
         const results = [];
-        fs.createReadStream('./pep_small.csv')
+        fs.createReadStream('./pep.csv')
             .pipe(csv())
             .on('data', (data) => results.push(data))
             .on('end', () => {
